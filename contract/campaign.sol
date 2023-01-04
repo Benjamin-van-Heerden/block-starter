d0b967ad7faf0190adc5ec0278ba3ab5e63c788b
Benjamin-van-Heerden <benjaminvh1997@gmail.com>
2023-01-05 00:21:54 +0200

    small change to contract. not done yet.

--- contract/campaign.sol
+++ contract/campaign.sol
@@ -38,12 +38,15 @@ contract Campaign {
         uint value, 
         address recipient
     ) public restricted {
-        Request newRequest = Request({
+
+        Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
             complete: false
         });
+
+        requests.push(newRequest);
     }
 
 }
