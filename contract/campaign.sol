9df466b10aba043f47786d046e510471a5f415a2
Benjamin-van-Heerden <benjaminvh1997@gmail.com>
2023-01-07 13:09:07 +0200

    campaign contract finished.

--- contract/campaign.sol
+++ contract/campaign.sol
@@ -10,12 +10,18 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        uint approvalsId;
     }
+
 
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    uint approvalsIdGlob = 0;
+    mapping(address => bool) public approvers;
+    mapping(uint => mapping(address => bool)) requestApprovals;
+    uint approversCount = 0;
 
     modifier restricted() {
         require(msg.sender == manager, "Only the manager may access this functionality");
@@ -30,23 +36,59 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value >= minimumContribution, string.concat("Need to contribute at least ", Strings.toString(minimumContribution), " wei"));
 
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approvalCount++;
     }
 
     function createRequest(
         string memory description, 
         uint value, 
         address recipient
-    ) public restricted {
+    ) public restricted {  
+        // value may not exceed the balance of the contract
+        require(address(this).balance >= value, "Not enough funds to complete the request.");
 
         Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0,
+            approvalsId: approvalsIdGlob
         });
-
+        // keep track of the index of the approvals
+        approvalsIdGlob++;
         requests.push(newRequest);
+    }
+
+    function approveRequest(uint requestIndex, bool vote) public {
+        Request storage activeRequest = requests[requestIndex]; 
+        mapping(address => bool) storage activeRequestApprovals = requestApprovals[activeRequest.approvalsId]; 
+
+        require(approvers[msg.sender], "Need to be a contributer to be able to vote.");
+        require(!activeRequestApprovals[msg.sender], "You have already voted on this request.");
+
+        // record the vote
+        activeRequestApprovals[msg.sender] = true;
+
+        // if the vote is true append to the count otherwise do nothing
+        if (vote) {
+            activeRequest.approvalCount += 1;
+        }
+    }
+
+    function finalizeRequest(uint requestIndex) public restricted {
+        Request storage activeRequest = requests[requestIndex];
+        // mapping(address => bool) storage activeRequestApprovals = requestApprovals[activeRequest.approvalsId]; 
+
+        require(!activeRequest.complete, "Request has already been finalized.");
+        require(activeRequest.approvalCount > (approversCount / 2), "Not enough votes to finalize the request.");
+
+        // send the money to the recipient
+        payable(activeRequest.recipient).transfer(activeRequest.value);
+
+        // mark the request as complete
+        activeRequest.complete = true;
     }
 
 }
